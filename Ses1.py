b01eee74d42a00a7a3089d22084718a0dbab1041
Abrahan-Perez <i2511689@continental.edu.pe>
2025-02-20 19:25:33 -0500

    Realizando nuestro primer código

---
+++ Ses1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+# Titulo de la aplicación
+st.title("Bienvanido a mi apliacación")
+
+#Solicitar nombre
+
+nombre = st.text_input("Por favor ingresa tu nombre")
+
+#Mostrar el saludo si el usuario ingresa su nombre
+
+if nombre:
+    st.write(f"Hola, {nombre}! Bienvenido a esta aplicación web de Streamlit")
