7779fbe4a958ea4d30418cbe0f2c9aacb97d4153
Abrahan-Perez <i2511689@continental.edu.pe>
2025-02-20 22:12:42 -0500

    Realizando cálculos para el negocio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Desarrollo_Aplicaciones_2025-2
 IC-2025-2
+https://ses1-da-2025-2.streamlit.app/
+https://ses2-da-2025-2.streamlit.app/
--- Ses2.py
+++ Ses2.py
@@ -1,3 +1,14 @@
 import streamlit as st
+from algoritmos import  AlgoritmosSecuenciales
 
 st.title ("Algoritmos Secuenciales con POO en Streamlit")
+
+#Entrada de usuario
+numero = st.number_input("Ingrese un número entero positivo", min_value=1, value=5, step=1)
+
+#Instanciamos la clase con el número ingresado
+algoritmos = AlgoritmosSecuenciales (numero)
+
+#botones para ejecutar los algoritmos secuenciales
+if st.button("Calcular la suma de  N números"):
+    st.success(f"La suma de los primeros {numero} número es: {algoritmos.suma_n_numeros}")
