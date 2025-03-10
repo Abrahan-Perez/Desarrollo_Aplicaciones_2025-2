9f5bd616239c0febeff3dc59e9b7d50bd90fee4a
Abrahan-Perez <i2511689@continental.edu.pe>
2025-03-09 22:49:44 -0500

    Agregando los primeros códigos

--- IDL2_Entregable.py
+++ IDL2_Entregable.py
@@ -1,1 +1,9 @@
 import streamlit as st
+
+st.title("Formulario de Productos - Confitería Dulcino")
+#Creando entrada para nombre del producto
+nombre_producto = st.text_input("Nombre del producto", max_chras=20)
+#Entrada para el precio
+precio = st.text_input("Precio del producto")
+#Lista de categorias permitidas
+categorias_permitidas = ["Chocolates", "Caramelos", "Mashmelo", "Galletas", "Salados", "Gomas de mascar"]
